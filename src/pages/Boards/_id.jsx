f5926c813afab2b78ebd9607688b282cbe239b34
quan <imhquan@gmail.com>
2024-03-04 21:55:10 +0700

    Fix things

---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,17 @@
+// Board Detail
+import Container from '@mui/material/Container'
+import AppBar from '../../components/AppBar/AppBar'
+import BoardBar from './BoardBar/BoardBar'
+import BoardContent from './BoardContent/BoardContent'
+
+function Board() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  )
+}
+
+export default Board
--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,117 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+// import { createTheme } from '@mui/material/styles'
+// import { red } from '@mui/material/colors'
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6'
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#556cd6'
+//     },
+//     secondary: {
+//       main: '#19857b'
+//     },
+//     error: {
+//       main: red.A400
+//     }
+//   }
+// })
+
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+//import { deepOrange, orange, teal, cyan } from '@mui/material/colors'
+
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+const COLUMN_HEADER_HEIGHT ='50px'
+const COLUMN_FOOTER_HEIGHT ='56px'
+
+// Create a theme instance.
+const theme = extendTheme({
+  trello: {
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    columnHeaderHeight: COLUMN_HEADER_HEIGHT,
+    columnFooterHeight: COLUMN_FOOTER_HEIGHT
+  },
+  colorSchemes: {
+    // light: {
+    //   palette: {
+    //     primary: teal,
+    //     secondary: deepOrange
+    //   }
+    // },
+    // dark: {
+    //   palette: {
+    //     primary: cyan,
+    //     secondary: orange
+    //   }
+    // }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#dcdde1',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: 'white'
+          }
+        }
+      }
+
     },
-    secondary: {
-      main: '#19857b'
+    // Name of the component
+    MuiButton: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          textTransform: 'none',
+          borderWidth: '0.5px',
+          '&:hover': { borderWidth: '0.5px' }
+        }
+      }
     },
-    error: {
-      main: red.A400
+    MuiInputLabel: {
+      styleOverrides: {
+        // Name of the slot
+        root:{ fontSize: '0.875rem' }
+      }
+    },
+    MuiTyprography: {
+      styleOverrides: {
+        // Name of the slot
+        root:{
+          '&.MuiTypography-body1': { fontSize: '0.875rem' }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        // Name of the slot
+        root:{
+          fontSize: '0.875rem',
+          '& fieldset': {
+            borderWidth: '0.5px !important'
+          },
+          '&:hover fieldset': {
+            borderWidth: '1px !important'
+          },
+          '&.Mui-focused fieldset': {
+            borderWidth: '1px !important'
+          }
+        }
+      }
     }
   }
 })
+
 
 export default theme
